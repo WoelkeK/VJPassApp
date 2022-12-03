e51bc61d7f2278121c2848627652a62b00f3ceb9
Krzysztof <krzysiek@woelke.pl>
2022-12-03 14:35:47 +0100

    Missed crud methods / endpoints for controller and service layers.

--- src/main/java/pl/woelke/vjpassapp/controller/PasswordApi.java
+++ src/main/java/pl/woelke/vjpassapp/controller/PasswordApi.java
@@ -1,9 +1,18 @@
 package pl.woelke.vjpassapp.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.woelke.vjpassapp.model.Dailypass;
+import pl.woelke.vjpassapp.service.DailypassService;
 
+import javax.lang.model.element.Name;
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -11,10 +20,55 @@ import java.util.logging.Logger;
 public class PasswordApi {
 
     private static Logger LOGGER = Logger.getLogger(PasswordApi.class.getName());
+    private final DailypassService dailypassService;
 
-    @GetMapping("/passwords")
-    public int getAllPasswords() {
+    public PasswordApi(DailypassService dailypassService) {
+        this.dailypassService = dailypassService;
+    }
+
+    @GetMapping("/test")
+    public int testApiEndpoint() {
         LOGGER.info("getAllPasswords()");
         return 200;
+    }
+
+    @GetMapping("/passwords")
+    public List<Dailypass> getAllPasswords() {
+        LOGGER.info("getAllPasswords()");
+        List<Dailypass> dailypassList = dailypassService.list();
+        LOGGER.info("getAllPasswords(...)");
+        return dailypassList;
+    }
+
+    @PostMapping("/passwords")
+    public Dailypass create(@RequestBody Dailypass dailypass) {
+        LOGGER.info("create()");
+        Dailypass savedDailyPass = dailypassService.create(dailypass);
+        LOGGER.info("create(...)");
+        return savedDailyPass;
+    }
+
+    @GetMapping("/passwords/{id}")
+    public Dailypass read(@PathVariable(name = "id") Long id) {
+        LOGGER.info("read(" + id + ")");
+        Dailypass singleDailyPass = dailypassService.read(id);
+        LOGGER.info("read(...) " + singleDailyPass);
+        return singleDailyPass;
+    }
+
+    @PutMapping("/passwords")
+    public Dailypass update(@RequestBody Dailypass dailypass) {
+        LOGGER.info("update()");
+        Dailypass updatedDailyPass = dailypassService.update(dailypass);
+        LOGGER.info("update(...) " + updatedDailyPass);
+        return updatedDailyPass;
+    }
+
+    @DeleteMapping("/passwords/{id}")
+    public String delete(@PathVariable Long id) {
+        LOGGER.info("delete()");
+        String deleteStatus = dailypassService.delete(id);
+        LOGGER.info("delete(...) " + deleteStatus);
+        return deleteStatus;
     }
 }
--- src/main/java/pl/woelke/vjpassapp/service/DailypassService.java
+++ src/main/java/pl/woelke/vjpassapp/service/DailypassService.java
@@ -1,9 +1,14 @@
 package pl.woelke.vjpassapp.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import pl.woelke.vjpassapp.model.Dailypass;
 import pl.woelke.vjpassapp.repository.DailypassRepository;
 
+import javax.lang.model.element.Name;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -24,19 +29,34 @@ public class DailypassService {
         return dailypasses;
     }
 
-    public void create() {
-
+    public Dailypass create(Dailypass dailypass) {
+        LOGGER.info("create()");
+        Dailypass savedDailyPass = dailypassRepository.save(dailypass);
+        LOGGER.info("create(...)");
+        return savedDailyPass;
     }
 
-    public void read() {
-
+    public Dailypass read(Long id) {
+        LOGGER.info("read(" + id + ")");
+        Dailypass singleDailyPass = dailypassRepository.findById(id).orElseThrow();
+        LOGGER.info("read(...)");
+        return singleDailyPass;
     }
 
-    public void update() {
-
+    public Dailypass update(Dailypass dailypass) {
+        LOGGER.info("update()");
+        Dailypass editedDailyPass = dailypassRepository.findById(dailypass.getId()).orElseThrow();
+        editedDailyPass.setDailypass(dailypass.getDailypass());
+        Dailypass updatedDailyPass = dailypassRepository.save(editedDailyPass);
+        LOGGER.info("update(...)");
+        return updatedDailyPass;
     }
 
-    public void delete() {
-
+    public String delete(Long id) {
+        LOGGER.info("delete(" + id + ")");
+        Dailypass deleteDailyPass = dailypassRepository.findById(id).orElseThrow();
+        dailypassRepository.delete(deleteDailyPass);
+        LOGGER.info("delete(...)");
+        return "delete complete";
     }
 }
