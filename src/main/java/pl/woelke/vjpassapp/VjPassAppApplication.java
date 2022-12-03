bdd4ac5845e21c14009b2b29b18223a61e33d176
Krzysztof <krzysiek@woelke.pl>
2022-12-03 10:58:31 +0100

    Init structure of VJ passwords app

---
+++ src/main/java/pl/woelke/vjpassapp/VjPassAppApplication.java
@@ -0,0 +1,13 @@
+package pl.woelke.vjpassapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjPassAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VjPassAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/woelke/vjpassapp/VjPassAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.woelke.vjpassapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjPassAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
